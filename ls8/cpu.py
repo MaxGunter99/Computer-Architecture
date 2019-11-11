2782548433b242162cdf6aa3ba362dc571ba0ac8
maxgunter99 <maxgunter99@gmail.com>
2019-11-11 16:52:31 -0600

    halt working

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,12 +10,18 @@ class CPU:
     def __init__(self):
 
         """Construct a new CPU."""
+
+        print( 'Init Called' )
+
+        self.Running_The_CPU = True
         self.ram = [0] * 256
         self.pc = 0
 
     def load(self):
 
         """Load a program into memory."""
+
+        print( 'Load Called' )
 
         address = 0
 
@@ -24,11 +30,11 @@ class CPU:
         program = [
 
             # From print8.ls8
-            0b10000010, # LDI R0,8 --- load "immediate", store a value in a register, or "set this register to this value".
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0 --- a pseudo-instruction that prints the numeric value stored in a register.
-            0b00000000,
+            # 0b10000010, # LDI R0,8 --- load "immediate", store a value in a register, or "set this register to this value".
+            # 0b00000000,
+            # 0b00001000,
+            # 0b01000111, # PRN R0 --- a pseudo-instruction that prints the numeric value stored in a register.
+            # 0b00000000,
             0b00000001, # HLT --- halt the CPU and exit the emulator.
         ]
 
@@ -36,10 +42,12 @@ class CPU:
             self.ram[address] = instruction
             address += 1
 
-
+    # Arithmetic Logic Unit
     def alu(self, op, reg_a, reg_b):
 
         """ALU operations."""
+
+        print( 'ALU Called' )
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
@@ -53,6 +61,8 @@ class CPU:
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
+
+        print( 'Trace Called' )
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
@@ -69,14 +79,29 @@ class CPU:
         print()
 
     # ram_read() should accept the address to read and return the value stored there.
-    def ram_read( address ):
-        pass
+    def ram_read( self , address ):
+        print( 'Ram_Read called' )
+        return self.ram[ address ]
 
     # raw_write() should accept a value to write, and the address to write it to.
-    def raw_write( value ):
+    def raw_write( self , value ):
+        print( 'raw_write called' )
         pass
 
     def run(self):
 
         """Run the CPU."""
-        pass
+
+        print( 'Running' )
+        print( self.ram[0] )
+        # ir or ram
+        # operand a and operand b
+
+        while self.Running_The_CPU:
+
+            command = self.ram[ 0 ]
+
+            if command == 0b00000001:
+                print( 'Halt' )
+                self.Running_The_CPU == False
+                return self.Running_The_CPU
