5c9b66824039a974fe37c425db3232c62f28dbe5
maxgunter99 <maxgunter99@gmail.com>
2019-11-13 17:07:19 -0600

    multiply is working

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -17,6 +17,8 @@ class CPU:
         self.ram = [0] * 256
         self.pc = 0
         self.reg = [0] * 8
+        self.SP = 7
+        self.reg[ self.SP] = 0xf4
 
         # print( 'Running:' , self.Running_The_CPU )
         # print( 'Random Access Memory:' , self.ram )
@@ -32,28 +34,26 @@ class CPU:
 
         print( '\ncall' , '\ninterrupts' , '\nkeyboard' , '\nmult' , '\nprint8' , '\nprintstr' , '\nsctest' , '\nstack' , '\nstackoverflow' )
         what_to_run = input( '\nWhat Do You Want To Run?\n' )
-        f = open( f'./examples/{str( what_to_run )}.ls8' , "r")
-        content = f.readlines()
 
         program = []
 
-        for i in content:
-            
-            if i[0] == '0':
-                line_stating_0 = int(i[:8])
-                program.append( line_stating_0 )
-            elif i[0] == '1':
-                line_stating_1 = int(i[:8])
-                program.append( line_stating_1 )
+        with open( f'./examples/{str( what_to_run )}.ls8' ) as f:
+            for line in f:
+
+                line = line.split("#")[0]
+                line = line.strip()  # lose whitespace
+
+                if line == '':
+                    continue
+
+                val = int(line)
+                self.ram[address] = val
+                address += 1
 
 
         # f.close()
 
         print( 'program' , program )
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
 
     # Arithmetic Logic Unit
     def alu(self, op, reg_a, reg_b):
@@ -66,7 +66,10 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
+
+        elif op == "MUL":
+            self.reg[ reg_a ] *= self.reg[ reg_b ]
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -103,21 +106,33 @@ class CPU:
         print( 'raw_write called' )
         pass
 
+    def binaryToDecimal( self , binary ): 
+      
+        binary1 = binary 
+        decimal, i, n = 0, 0, 0
+        while(binary != 0): 
+            dec = binary % 10
+            decimal = decimal + dec * pow(2, i) 
+            binary = binary//10
+            i += 1
+        return decimal
+
     def run(self):
 
         """Run the CPU."""
 
         print( '\nRunning' )
 
+        print( self.ram  , '\n' )
+
         while self.Running_The_CPU:
 
             # For Debugging 🕷
             # self.trace()
+            print( '\n' )
 
             index = self.pc
             command = self.ram[ index ]
-
-            print( '\nStep:' , self.pc , '⬇︎' )
 
             if command == 0b00000001:
                 print( 'HLT 🛑\n' )
@@ -126,26 +141,41 @@ class CPU:
 
             elif command == 0b00000000:
                 print( 'NOP' )
-                print( '---> No operation. Do nothing for this instruction. 💤' )
+                print( '---> No operation. 💤' )
                 self.pc += 1
 
-            elif command == 0b10000010:
-                print( 'LDI' )
-                value1 = self.ram[ self.pc + 1 ]
-                self.reg.insert( 0 , value1 )
-                self.reg.pop()
+            elif command == 10000010:
 
-                value2 = self.ram[ self.pc + 2 ]
-                self.reg.insert( 0 , value2 )
-                self.reg.pop()
+                print( 'LDI' )
+                # Register number
+                this_register = self.ram[ self.pc + 1 ]
+
+                # Value to put in that register ( also convert to decimal )
+                original = self.ram[ self.pc + 2 ]
+                value = self.binaryToDecimal( original )
+                # Put Value In Register
+                self.reg[ this_register ] = value
+
                 self.pc += 3
 
-            elif command == 0b01000111:
+            elif command == 1000111:
+
                 print( 'PRN' )
-                print( '---> Register[0] value:' , self.reg[0] )
-                self.pc += 1
+                this_register = self.ram[ self.pc + 1 ]
+                print( '---> value:' , self.reg[0] )
+                self.pc += 2
+
+            elif command == 10100010:
+
+                print( 'MULT' )
+                register_one = self.ram[ self.pc + 1 ]
+                register_two = self.ram[ self.pc + 2 ]
+                self.alu( 'MUL' , register_one , register_two )
+
+                self.pc += 3
             
             else:
-                print( command )
+                print( f'{command} not set up yet' )
                 self.Running_The_CPU == False
                 return self.Running_The_CPU
+
